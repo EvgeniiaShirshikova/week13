6a27f546498a9c7afcc329d58508d51d2edeafb9
EvgeniiaShirshikova <shirshikovae@gmail.com>
2023-03-27 11:12:00 -0600

    commit

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-const inpname = document.getElementById('inpname');
-const inpimg = document.getElementById('inpimg');
-const inpcomment = document.getElementById('inpcomment');
+const inputName = document.getElementById('inpname');
+const inputImage = document.getElementById('inpimg');
+const inputComment = document.getElementById('inpcomment');
 const form = document.querySelector('form');
-const chatarea = document.querySelector('.chatarea');
+const chatArea = document.querySelector('.chatarea');
 const chatTitle = document.querySelector('.chattitle');
-const inpyes = document.getElementById('inpyes');
-const inpno = document.getElementById('inpno');
+const inputYes = document.getElementById('inpyes');
+const inputNo = document.getElementById('inpno');
 let icons = ['./assets/icons/astronaut.png', './assets/icons/bear.png', './assets/icons/cat.png', './assets/icons/dog.png', './assets/icons/panda.png', './assets/icons/rabbit.png'];
 
 function checkSpam() {
-	let comment = inpcomment.value;
+	let comment = inputComment.value;
 	let checkedComment = comment.replace(/viagra|xxx/gi, '***');
 	return checkedComment;
 }
@@ -21,19 +21,18 @@ function createPost() {
 
 	const messageAvatar = document.createElement('img');
 	messageAvatar.classList.add('avatarimg');
-	if (inpimg.value === "") {
+	if (inputImage.value === "") {
 		let randomAvatar = Math.floor(Math.random() * icons.length);
 		messageAvatar.src = icons[randomAvatar];		
 	} else {
-		messageAvatar.src = inpimg.value;
+		messageAvatar.src = inputImage.value;
 	}
-	
 	post.append(messageAvatar);
 
 	const messageUserName = document.createElement('p');
 	messageUserName.classList.add('username');
-	if (inpyes.checked) {
-		messageUserName.textContent = inpname.value;
+	if (inputYes.checked) {
+		messageUserName.textContent = inputName.value;
 	} else {
 		messageUserName.textContent = "Username";
 	};	
@@ -48,17 +47,19 @@ function createPost() {
 	messageDate.classList.add('date');
 	messageDate.textContent = new Date();
 	post.append(messageDate);
+
+	form.reset();
 }
 
-inpname.addEventListener('input', () => {
-	inpname.value = inpname.value.replace(/( |^)[а-яёa-z]/g, function (u) {
+inputName.addEventListener('input', () => {
+	inputName.value = inputName.value.replace(/( |^)[а-яёa-z]/g, function (u) {
 		return u.toUpperCase();
 	});
 });
 
 form.addEventListener('submit', (event) => {
 	event.preventDefault();
-	if ((inpyes.checked && !(inpno.checked)) || (!(inpyes.checked) && inpno.checked)) {
+	if ((inputYes.checked && !(inputNo.checked)) || (!(inputYes.checked) && inputNo.checked)) {
 		createPost()
 	} else {
 		alert("Укажите показывать ли ваше имя!");
